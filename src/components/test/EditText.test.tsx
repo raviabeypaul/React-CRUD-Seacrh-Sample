026e09c1d1577f33efbe2ca2ae67440f53dbef2e
ravi <raviabeypaul@gmail.com>
2023-12-04 00:36:29 -0500

    fixed tests

--- src/components/test/EditText.test.tsx
+++ src/components/test/EditText.test.tsx
@@ -7,18 +7,18 @@ import { EditText } from '../EditText';
 describe('Testing EditText', () => {
     const editTextValue = "Hello this is a EditText";
     test('Styling with Default Field Type Width', () => {
-        render(<EditText textfieldtype={'default'} value={editTextValue}/>)
+        render(<EditText id={"1"} textfieldtype={'default'} value={editTextValue}/>)
         const label = screen.getByDisplayValue(editTextValue)
-        const editText = screen.getByTestId('editText');
+        const editText = screen.getByTestId('editText1');
         const styles = getComputedStyle(editText)
         expect(styles.width).toBe('200px')
         expect(label).toBeInTheDocument()
     })
 
-    test('Styling Fullwidth Field Type', () => {
-        render(<EditText textfieldtype='fullwidth' value={editTextValue}/>)
-        const editText = screen.getByTestId('editText');
-        expect(editText.style.width).tobe('100%')
+    test('Styling  Fullwidth Field Type', () => {
+        render(<EditText  id={"1"} textfieldtype='fullwidth' value={editTextValue}/>)
+        const editText = screen.getByTestId('editText1');
+        expect(editText.style.width).toEqual('100%')
         const label = screen.getByDisplayValue(editTextValue)
         expect(label).toBeInTheDocument()
     })
