9840ebff5241139c528d70b3ef1c51cf3423eb36
ravi <raviabeypaul@gmail.com>
2023-12-13 18:16:20 -0500

    update e2e

--- tests/master.spec.ts
+++ tests/master.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 /* eslint-disable testing-library/prefer-screen-queries */
 test('Testing Edit Functionality', async ({ page }) => {
-  await page.goto('https://raviabeypaul.github.io/React-CRUD-Seacrh-Sample/');
+  await page.goto('http://ec2-3-82-108-46.compute-1.amazonaws.com:3000/');
   await page.getByTestId("edit_0").click();
   await page.getByLabel("E-Mail").fill("test@gmail.com");
   await page.getByTestId("reservation-save-close").click();
